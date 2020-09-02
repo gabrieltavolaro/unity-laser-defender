41592c20df7ae3e8799aacd693cd2ea03e877ecc
Gabriel Tavolaro <gabriel.tavolaro@hotmail.com>
2020-09-02 09:17:41 -0300

    Add movement boundries

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,11 +6,31 @@ public class Player : MonoBehaviour
 {
 
     // Configuration Parameters
-    [SerializeField] float moveSpeed = 10;
+    [SerializeField] float moveSpeed = 10f;
+    [SerializeField] float padding = 1f;
+
+    // Variables
+    float xMin, xMax;
+    float yMin, yMax;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        SetUpMoveBoundries();
+    }
+
+    private void SetUpMoveBoundries()
+    {
+        // Create local camera variable
+        Camera gameCamera = Camera.main;
+
+        // Get 'x' min and max
+        xMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).x + padding;
+        xMax = gameCamera.ViewportToWorldPoint(new Vector3(1, 0, 0)).x - padding;
+
+        // Get 'y' min and max
+        yMin = gameCamera.ViewportToWorldPoint(new Vector3(0, 0, 0)).y + padding;
+        yMax = gameCamera.ViewportToWorldPoint(new Vector3(0, 1, 0)).y - padding;
     }
 
     // Update is called once per frame
@@ -21,13 +41,13 @@ public class Player : MonoBehaviour
 
     private void Move()
     {
-        // Get 'x' movement
+        // Get and set 'x' movement
         float deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
-        float newXPos = transform.position.x + deltaX;
+        float newXPos = Mathf.Clamp(transform.position.x + deltaX, xMin, xMax);
 
-        // Get 'y' movement
+        // Get and set 'y' movement
         float deltaY = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
-        float newYPos = transform.position.y + deltaY;
+        float newYPos = Mathf.Clamp(transform.position.y + deltaY, yMin, yMax);
 
         // Add to position
         transform.position = new Vector2(newXPos, newYPos);
