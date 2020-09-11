6870bbd76cdeea9c4cf87db421957f77b76d5c9f
Gabriel Tavolaro <gabriel.tavolaro@hotmail.com>
2020-09-11 12:51:07 -0300

    Enemy Laser Set Up

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -6,17 +6,41 @@ public class Enemy : MonoBehaviour
 {
     // Configuration Parameters
     [SerializeField] float health = 100;
+    [SerializeField] float shotCounter;
+
+    [SerializeField] float minTimeBetweenShots = 0.2f;
+    [SerializeField] float maxTimeBetweenShots = 3f;
+
+    [SerializeField] GameObject projectile;
+    [SerializeField] float projectileSpeed = 10f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        shotCounter = Random.Range(minTimeBetweenShots, maxTimeBetweenShots);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        CountDownAndShoot();
+    }
+
+    private void CountDownAndShoot()
+    {
+        shotCounter -= Time.deltaTime;
+        if (shotCounter <= 0f)
+        {
+            Fire();
+            shotCounter = Random.Range(minTimeBetweenShots, maxTimeBetweenShots);
+        }
+    }
+
+    private void Fire()
+    {
+        // Instantiate laser
+        GameObject laser = Instantiate(projectile, transform.position, Quaternion.identity) as GameObject;
+        laser.GetComponent<Rigidbody2D>().velocity = new Vector2(0, -projectileSpeed);
     }
 
     private void OnTriggerEnter2D(Collider2D other)
